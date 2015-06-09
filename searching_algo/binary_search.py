6df705557bf42cc50d73082a9d1ba51199e4a809
Shrikant <shrikantah@gmail.com>
2015-06-09 10:52:05 +0530

    searching algo

---
+++ searching_algo/binary_search.py
@@ -0,0 +1,24 @@
+
+
+
+
+def binary_search(item_list, item):
+    first = 0
+    last = len(item_list) - 1
+    found = False
+
+    while first <= last and not found:
+                logger.info(next_line)
+        mid = (first + last) // 2
+        if item_list[mid] == item:
+            found = True
+        else:
+            if item < item_list[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return found
+
+if __name__ == "__main__":
+    s_list = [1,2,3,4,5,6,7]
+    print binary_search(s_list, 5)
